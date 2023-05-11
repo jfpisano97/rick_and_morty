579921f1c9af1d4de4aeeff2578cfc3627b62d1a
jfpisano97 <jfpisano97@gmail.com>
2023-05-11 20:15:17 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import style from './App.module.css';
 import Cards from './components/Cards/Cards.jsx';
 import SearchBar from './components/SearchBar/SearchBar.jsx';
 import characters from './data.js';
 
 function App() {
    return (
-      <div className='App'>
+      <div className={style.App}>
          <SearchBar onSearch={(characterID) => window.alert(characterID)} />
          <Cards characters={characters} />
       </div>
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,9 +2,12 @@ import style from './SearchBar.module.css';
 
 export default function SearchBar(props) {  // la función onSearch está en props, se pasa por props en App.js, entonces si la quiero usar tengo que hacer props.onSearch
    return (
-      <div>
-         <input className ={style.input} type='search' /> 
-         <button className={style.addButton} onClick={(id) => {props.onSearch(id)}}>Add</button> 
+      <div className={style.bar}>
+         <div className={style.logo}>Ricardo Mort</div>
+         <div className={style.search}>
+            <input className ={style.input} type='search' /> 
+            <button className={style.addButton} onClick={(id) => {props.onSearch(id)}}>Add</button> 
+         </div>
       </div>
    );
 }
