1036d8ec801eb1603fe6ac12b7dbb5c3ff54d10f
jfpisano97 <jfpisano97@gmail.com>
2023-05-30 20:05:01 -0300

    hooks incomplete

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,13 +1,18 @@
-import { ADD_FAV, REMOVE_FAV } from './actions';
+import { ADD_FAV, REMOVE_FAV, FILTER, ORDER } from './actions';
 
 const initialState = {
     myFavorites: [],
+    allCharacters: [],
 };
 
 const rootReducer = (state = initialState, action) => {
     switch (action.type){
         case ADD_FAV:
-            return {...state, myFavorites:[...state.myFavorites, action.payload]};
+            return {
+                ...state, 
+                myFavorites:[...state.myFavorites, action.payload],
+                allCharacters:[...state.myFavorites, action.payload],
+            };
         case REMOVE_FAV:
             return {
                 ...state, 
@@ -15,8 +20,47 @@ const rootReducer = (state = initialState, action) => {
                     (char) => char.id !== parseInt(action.payload)
                 ),
             };
+        case FILTER:
+            return {
+                ...state,
+                allCharacters: state.allCharacters.filter(
+                    (char) => char.gender === action.payload
+                ),
+                myFavorites: state.allCharacters,
+            }
+        case ORDER:
+            return {
+                ...state,
+                allCharacters: action.payload === 'A' ? state.allCharacters.sort() : state.allCharacters.sort((a, b) => b - a),
+                myFavorites: state.allCharacters,
+            }
         default: return {...state};
     }
 };
 
 export default rootReducer;
+
+
+// import { ADD_FAV, REMOVE_FAV, FILTER, ORDER } from './actions';
+
+// const initialState = {
+//     myFavorites: [],
+//     allCharacters: [],
+// };
+
+// const rootReducer = (state = initialState, action) => {
+//     switch (action.type){
+//         case ADD_FAV:
+//             return {...state, myFavorites:[...state.myFavorites, action.payload]};
+//         case REMOVE_FAV:
+//             return {
+//                 ...state, 
+//                 myFavorites: state.myFavorites.filter(
+//                     (char) => char.id !== parseInt(action.payload)
+//                 ),
+//             };
+//         default: return {...state};
+//     }
+// };
+
+// export default rootReducer;
--- src/views/Favorites/Favorites.jsx
+++ src/views/Favorites/Favorites.jsx
@@ -1,25 +1,52 @@
 import { connect } from 'react-redux';
 import Card from '../../components/Card/Card';
 import style from './Favorites.module.css';
+import { orderCards, filterCards } from '../../redux/actions';
+import { useDispatch } from 'react-redux';
+
 
 function Favorites(props){
+    
+    const dispatch = useDispatch();
+    const handleOrder = () => {
+        dispatch(orderCards())
+    };
+    const handleFilter = () => {
+        dispatch(filterCards())
+    };
 
 
     return(
-    <div className={style.favs}>
-        {props.myFavorites.map(({id, name, status, species, gender, origin, image}) => {
-            return (<Card 
-                id = {id}
-                name = {name}
-                status = {status}
-                species = {species}
-                gender = {gender}
-                origin = {origin}
-                image = {image}
-                onClose = {props.onClose}
-            />)
-        })}
-    </div>
+        <div>
+            <div>
+                <select>
+                    <option value='A'>Ascendent order</option>
+                    <option value='D'>Descendent order</option>
+                </select>
+                <select>
+                    <option value='All'>All</option>
+                    <option value='Male'>Male</option>
+                    <option value='Female'>Female</option>
+                    <option value='Genderless'>Genderless</option>
+                    <option value='unknown'>unknown</option>
+                </select>
+            </div>
+            <div className={style.favs}>
+                
+                {props.myFavorites.map(({id, name, status, species, gender, origin, image}) => {
+                    return (<Card 
+                        id = {id}
+                        name = {name}
+                        status = {status}
+                        species = {species}
+                        gender = {gender}
+                        origin = {origin}
+                        image = {image}
+                        onClose = {props.onClose}
+                    />)
+                })}
+            </div>
+        </div>
     )
 }
 
